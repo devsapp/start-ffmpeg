1f7484b2e3360cd452a9ab7136d9ccef969053b8
anycodes <service@52exe.cn>
2021-12-10 19:02:53 +0800

    update info

--- ffmpeg-app/hook/index.js
+++ ffmpeg-app/hook/index.js
@@ -1,8 +1,4 @@
 async function preInit(inputObj) {
-
-}
-
-async function postInit(inputObj) {
     console.log(`\n     _______  _______  __   __  _______  _______  _______ 
     |       ||       ||  |_|  ||       ||       ||       |
     |    ___||    ___||       ||    _  ||    ___||    ___|
@@ -11,12 +7,16 @@ async function postInit(inputObj) {
     |   |    |   |    | ||_|| ||   |    |   |___ |   |_| |
     |___|    |___|    |_|   |_||___|    |_______||_______|
                                         `)
+}
+
+async function postInit(inputObj) {
     console.log(`\n    Welcome to the ffmpeg-app application
      This application requires to open these services: 
          FC : https://fc.console.aliyun.com/
-     This application can help you quickly deploy the ffmpeg-app project.
-     The application uses FC component：https://github.com/devsapp/fc
-     The application homepage: https://github.com/devsapp/ffmpeg-app\n`)
+         OSS: https://oss.console.aliyun.com/
+         
+     * 关于项目的介绍，可以参考：https://github.com/devsapp/start-ffmpeg/tree/master/ffmpeg-app/src
+     * 项目初始化完成，您可以直接进入项目目录下，并使用 s deploy 进行项目部署\n`)
 }
 
 module.exports = {
--- rtmp-snapshot/hook/index.js
+++ rtmp-snapshot/hook/index.js
@@ -1,8 +1,4 @@
 async function preInit(inputObj) {
-
-}
-
-async function postInit(inputObj) {
     console.log(`\n     _______  _______  __   __  _______  _______  _______ 
     |       ||       ||  |_|  ||       ||       ||       |
     |    ___||    ___||       ||    _  ||    ___||    ___|
@@ -11,12 +7,20 @@ async function postInit(inputObj) {
     |   |    |   |    | ||_|| ||   |    |   |___ |   |_| |
     |___|    |___|    |_|   |_||___|    |_______||_______|
                                         `)
+}
+
+async function postInit(inputObj) {
     console.log(`\n    Welcome to the ffmpeg-app application
      This application requires to open these services: 
          FC : https://fc.console.aliyun.com/
-     This application can help you quickly deploy the ffmpeg-app project.
-     The application uses FC component：https://github.com/devsapp/fc
-     The application homepage: https://github.com/devsapp/start-ffmpeg\n`)
+         OSS: https://oss.console.aliyun.com/
+         
+     * 关于项目的介绍，可以参考：https://github.com/devsapp/start-ffmpeg/blob/master/rtmp-snapshot/src
+     * 项目初始化完成，您可以直接进入项目目录下
+        1. 进行构建：s build --use-docker --dockerfile ./code/Dockerfile
+        2. 项目部署：s deploy --use-local -y
+     * 最后您还可以验证项目的正确性： 
+        https://github.com/devsapp/start-ffmpeg/tree/master/rtmp-snapshot/src#%E6%B5%8B%E8%AF%95\n`)
 }
 
 module.exports = {
